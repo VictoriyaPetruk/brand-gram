931586541f1669c4b9560e26f1e367927609dafd
Viktoriia <69762201+VictoriyaPetruk@users.noreply.github.com>
2025-05-20 17:33:28 +0200

    Update generatePrompt.ts

--- src/app/analytics/[accountName]/generatePrompt.ts
+++ src/app/analytics/[accountName]/generatePrompt.ts
@@ -1,6 +1,6 @@
 const generatePrompt = (jsonContent: string): string => {
 
-      const prompt = `Pretend you are smm. Generate the content for the Instagram account. Write a post and generate an image later.
+      const prompt = `Pretend you are smm. Generate the content for the Instagram account. Write a post and generate an image next.
         Information about the Instagram page: ${jsonContent}
       `;
       
